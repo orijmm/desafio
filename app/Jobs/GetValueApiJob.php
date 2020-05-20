487b2fc6e76489b2291e654b7243d1476e9f2a7c
orijmm <orianajmm@gmail.com>
2020-05-20 15:08:48 -0400

    if job

--- app/Jobs/GetValueApiJob.php
+++ app/Jobs/GetValueApiJob.php
@@ -35,22 +35,29 @@ class GetValueApiJob implements ShouldQueue
      */
     public function handle(Request $request)
     {
+        $checkOldPay = NULL;
         if (is_null($this->paymentdate)) {
             $apiUrl = 'https://mindicador.cl/api/dolar/';
         } else {
+            $checkOldPay = Payment::where('payment_date',$this->paymentdate)->first();
             $date = \Carbon\Carbon::createFromFormat('Y-m-d',$this->paymentdate)->format('d-m-Y');
             $apiUrl = 'https://mindicador.cl/api/dolar/'.$date;
         }
-        
-        $curl = curl_init($apiUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $json = curl_exec($curl);
-        curl_close($curl);
+
+        if(is_null($checkOldPay)){
+            $curl = curl_init($apiUrl);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $json = curl_exec($curl);
+            curl_close($curl);
+
+            $decode = json_decode($json,true);
+            $usd_value = $decode['serie'][0]['valor'];
+        }else{
+            $usd_value = $checkOldPay->clp_usd;
+        }
             
-        $decode = json_decode($json,true);
-    
         $updatePayment = Payment::find($this->paymentid);
-        $updatePayment->clp_usd = $decode['serie'][0]['valor'];
+        $updatePayment->clp_usd = $usd_value;
         $updatePayment->save();
     }
 }
